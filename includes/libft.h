89e1c6ff12044520cd69c5eb6cfdc494d14111be
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-05-20 13:47:59 +0200

    Moved libft to new directory 'includes'

--- includes/libft.h
+++ includes/libft.h
@@ -6,13 +6,15 @@
 /*   By: bbrassar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 12:09:53 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/20 12:49:06 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/20 12:51:14 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+
+int		ft_isdigit(int c);
 
 int		ft_isspace(int c);
 
