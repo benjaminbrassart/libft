265859a37cb72d773ef56c20f62833ab3401f8ce
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-05-20 14:27:27 +0200

    Added ft_isalnum

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/20 12:54:24 by bbrassar          #+#    #+#              #
-#    Updated: 2021/05/20 14:25:23 by bbrassar         ###   ########.fr        #
+#    Updated: 2021/05/20 14:27:15 by bbrassar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,6 +25,7 @@ DIR_SRCS			= src
 DIR_INCLUDES		= includes
 
 SRCS				= $(addprefix ft_, $(addsuffix .c, \
+						isalnum \
 						isalpha \
 						isdigit \
 						isspace \
--- src/ft_isalnum.c
+++ src/ft_isalnum.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbrassar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/20 12:09:53 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/20 14:26:58 by bbrassar         ###   ########.fr       */
+/*   Created: 2021/05/20 14:23:42 by bbrassar          #+#    #+#             */
+/*   Updated: 2021/05/20 14:27:04 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
+#include "libft.h"
 
-int		ft_isalnum(int c);
-
-int		ft_isalpha(int c);
-
-int		ft_isdigit(int c);
-
-int		ft_isspace(int c);
-
-size_t	ft_strlen(char const *s);
-
-#endif
+int	ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
