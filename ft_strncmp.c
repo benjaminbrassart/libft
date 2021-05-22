1e8f62a8bd986eff6fcd0b288438c639f1162c18
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-05-22 16:55:56 +0200

    Fixed norme issue in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 13:41:22 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/22 13:44:44 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/22 16:37:32 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_strncmp(char const *s1, char const *s2, size_t n)
 {
 	unsigned char const	*b1;
-	unsigned char const *b2;
+	unsigned char const	*b2;
 
 	b1 = (unsigned char const *)s1;
 	b2 = (unsigned char const *)s2;
