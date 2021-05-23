0e76b649bda50ecca52f616b4ccd91deb542aa97
Benjamin BRASSART <bbrassar@student.42.fr>
2021-05-23 15:00:14 +0200

    Fixed ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 22:43:41 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/22 22:45:17 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/23 14:57:58 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 void	ft_lstadd_back(t_list **alst, t_list *new)
 {
-	if (*alst)
-		(*alst)->next = new;
+	t_list	*lst;
+
+	lst = ft_lstlast(*alst);
+	if (lst)
+		lst->next = new;
 	else
 		*alst = new;
 }
