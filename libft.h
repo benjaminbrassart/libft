9ac381716f2376d20270c5344215e513b77ab65c
Benjamin BRASSART <bbrassar@student.42.fr>
2021-05-23 14:47:09 +0200

    Fixed type conflict in libft.h for ft_lstmap

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 09:37:02 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/23 12:23:34 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/23 14:45:38 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 
-t_list	ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
