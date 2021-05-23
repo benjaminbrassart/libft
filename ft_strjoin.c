3e42435eabe50197fc4033b2b924ccde02d12d41
Benjamin BRASSART <bbrassar@student.42.fr>
2021-05-23 15:27:58 +0200

    Added 1 byte in calloc for ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 11:12:42 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/22 20:21:26 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/23 15:27:29 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 
 	len = ft_strlen(s1) + ft_strlen(s2);
-	str = ft_calloc(len, sizeof (char));
+	str = ft_calloc(len + 1, sizeof (char));
 	if (str)
 	{
 		ft_concat(str, s1);
