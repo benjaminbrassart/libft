a14b620b435b8df9e799986aefa318b45f37bfdc
Benjamin BRASSART <bbrassar@student.42.fr>
2021-05-23 14:14:14 +0200

    Fixed norme issue on ft_lstlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 22:34:44 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/22 22:36:38 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/23 14:13:57 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 t_list	*ft_lstlast(t_list *lst)
 {
 	if (lst && lst->next)
-		return ft_lstlast(lst->next);
+		return (ft_lstlast(lst->next));
 	return (lst);
 }
