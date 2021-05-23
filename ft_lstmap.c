4ea80a0b675e87c1aca8577f18cfe886bbb4c69e
Benjamin BRASSART <bbrassar@student.42.fr>
2021-05-23 15:28:53 +0200

    Fixed norme issue in ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,13 +6,13 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 14:35:12 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/23 14:52:03 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/23 15:28:43 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*temp;
