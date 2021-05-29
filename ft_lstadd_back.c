4af1db7c158d22c54f8e4a0703c4ad50ddb44acc
Benjamin BRASSART <bbrassar@e3r9p8.42.fr>
2021-05-29 12:34:09 +0200

    Securized a bit some functions

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 22:43:41 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/23 14:57:58 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/05/29 12:08:15 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 {
 	t_list	*lst;
 
-	lst = ft_lstlast(*alst);
-	if (lst)
-		lst->next = new;
-	else
-		*alst = new;
+	if (alst)
+	{
+		lst = ft_lstlast(*alst);
+		if (lst)
+			lst->next = new;
+		else
+			*alst = new;
+	}
 }
