f134e4f9d864969f479746cd26bf5a68343fccc5
Benjamin BRASSART <bbrassar@student.42.fr>
2021-07-29 10:53:07 +0200

    Added ft_bool to ft_def.h

--- include/ft_def.h
+++ include/ft_def.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 14:04:45 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/07/29 10:42:31 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/07/29 10:52:15 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,16 @@
 #  define FT_NULL ((void *)0)
 # endif /* FT_NULL */
 
+typedef _Bool			t_bool;
+
 typedef unsigned long	t_size;
 
 typedef long			t_ssize;
+
+typedef enum e_bool
+{
+	false = 0,
+	true = 1
+}	t_bool;
 
 #endif /* FT_DEF_H */
