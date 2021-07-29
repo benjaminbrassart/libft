15ab437d05dd626995e90baaa65fce530e53543d
Benjamin BRASSART <bbrassar@student.42.fr>
2021-07-29 10:43:00 +0200

    Changed NULL to FT_NULL

--- include/ft_def.h
+++ include/ft_def.h
@@ -6,16 +6,16 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 14:04:45 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/07/22 14:15:04 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/07/29 10:42:31 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_DEF_H
 # define FT_DEF_H
 
-# ifndef NULL
-#  define NULL ((void *)0)
-# endif /* NULL */
+# ifndef FT_NULL
+#  define FT_NULL ((void *)0)
+# endif /* FT_NULL */
 
 typedef unsigned long	t_size;
 
