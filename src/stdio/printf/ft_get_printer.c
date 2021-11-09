3942f09edd1e809c5e2c3a0ecbaf957313429e01
Benjamin BRASSART <bbrassar@student.42.fr>
2021-11-09 14:59:33 +0100

    Aligned variables in ft_get_printer (norminette)

--- src/stdio/printf/ft_get_printer.c
+++ src/stdio/printf/ft_get_printer.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 23:55:08 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/11/09 14:37:28 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/11/09 14:57:44 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_printer	ft_get_printer(int chr)
 {
 	t_printer *const	printers = ft_get_printers();
-	int	i;
+	int					i;
 
 	i = 0;
 	while (printers[i].chr != chr && printers[i].chr)
