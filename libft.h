a22294c3f75fa6aa91a340bbd1e0910b6b729d29
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-09 15:13:36 +0100

    Added INT_MAX_DIGITS macro

--- libft.h
+++ libft.h
@@ -6,12 +6,15 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 09:37:02 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/29 12:36:09 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/09 14:46:21 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# define INT_MAX_DIGITS 11
+
 # include <string.h>
 
 void	*ft_memset(void *b, int c, size_t len);
@@ -81,29 +84,5 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 
 void	ft_putnbr_fd(int n, int fd);
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
-t_list	*ft_lstnew(void *content);
-
-void	ft_lstadd_front(t_list **alst, t_list *new);
-
-int		ft_lstsize(t_list *lst);
-
-t_list	*ft_lstlast(t_list *lst);
-
-void	ft_lstadd_back(t_list **alst, t_list *new);
-
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
