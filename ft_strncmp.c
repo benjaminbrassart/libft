3f1863b8c4b7205b87da7847bb1a3f502b0b2481
Benjamin BRASSART <bbrassar@student.42.fr>
2021-12-09 15:13:48 +0100

    Changed decrementation order in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/22 13:41:22 by bbrassar          #+#    #+#             */
-/*   Updated: 2021/05/29 12:20:23 by bbrassar         ###   ########.fr       */
+/*   Updated: 2021/12/09 14:25:16 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strncmp(char const *s1, char const *s2, size_t n)
 		return (0);
 	b1 = (unsigned char const *)s1;
 	b2 = (unsigned char const *)s2;
-	while (*b1 && *b2 && *b1 == *b2 && --n)
+	while (--n && *b1 && *b2 && *b1 == *b2)
 	{
 		b1++;
 		b2++;
