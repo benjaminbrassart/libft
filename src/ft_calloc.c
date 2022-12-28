1ba9f4f1cabf7a940c45bdea501268b2665c0fda
Benjamin Brassart <bbrassar@student.42.fr>
2022-12-28 09:38:28 +0100

    Add check for overflow in ft_calloc

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,17 +6,24 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 14:55:25 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/05/18 20:27:50 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/12/28 09:38:05 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "ft.h"
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 
+	if (count > (ULONG_MAX / size))
+	{
+		errno = ERANGE;
+		return (NULL);
+	}
 	mem = malloc(count * size);
 	if (mem != NULL)
 		ft_memset(mem, 0, count * size);
