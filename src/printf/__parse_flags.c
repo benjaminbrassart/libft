92c95fce2825692f92a6bf83ac46718425209e04
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-09 19:50:13 +0200

    Use option table for flag parsing

--- src/printf/__parse_flags.c
+++ src/printf/__parse_flags.c
@@ -6,22 +6,35 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 08:17:31 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/17 10:20:04 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:03:40 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/options.h"
 
-// TODO not implemented
+static int const	g_flags_table[1 << (sizeof (char) * 8)] = {
+	OPT_NONE,
+['-'] = OPT_DASH,
+['0'] = OPT_ZERO,
+['+'] = OPT_PLUS,
+[' '] = OPT_SPACE,
+['#'] = OPT_HASHTAG,
+};
+
 void	__parse_flags(t_opt *opt, char const **fmt)
 {
-	char const *s = *fmt;
+	char const	*s = *fmt;
+	int			flag;
 
 	while (1)
 	{
-		if (*s == '#')
-			opt->bits |= OPT_HASHTAG;
-		else if (*s == '')
+		if (*s <= 0)
+			break ;
+		flag = g_flags_table[(int)*s];
+		if (flag == OPT_NONE)
+			break ;
+		opt->bits |= flag;
+		++s;
 	}
 	*fmt = s;
 }
