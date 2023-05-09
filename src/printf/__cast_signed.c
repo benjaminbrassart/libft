91840f564c26309d15deccd7dda23f0a00ff456a
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-09 19:47:41 +0200

    Explicitly use `signed` integers for __cast_signed

--- src/printf/__cast_signed.c
+++ src/printf/__cast_signed.c
@@ -6,22 +6,22 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 08:05:37 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/17 08:06:35 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:26:19 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/cast.h"
 
 long long	__cast_signed(enum e_lengthmod length,
-		long long n)
+		signed long long n)
 {
 	if (length == LEN_SHORT_SHORT)
 		return ((signed char)n);
 	if (length == LEN_SHORT)
-		return ((short)n);
+		return ((signed short)n);
 	if (length == LEN_NORMAL)
-		return ((int)n);
+		return ((signed int)n);
 	if (length == LEN_LONG)
-		return ((long)n);
-	return ((long long)n);
+		return ((signed long)n);
+	return ((signed long long)n);
 }
