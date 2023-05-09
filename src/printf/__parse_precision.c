cce239feefc3af204b8d12f082d63e05705a623f
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-09 19:51:10 +0200

    Add `*` for precision and field width

--- src/printf/__parse_precision.c
+++ src/printf/__parse_precision.c
@@ -6,21 +6,27 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 08:19:02 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/05/09 16:46:50 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:46:42 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/options.h"
 #include "ft.h"
 
-void	__parse_precision(t_opt *opt, char const **fmt)
+void	__parse_precision(t_opt *opt, va_list ap, char const **fmt)
 {
 	char const	*s = *fmt;
 	size_t		precision;
 
 	if (*s != '.')
 		return ;
-	++s;
+	if (*++s == '*')
+	{
+		*fmt = s + 1;
+		opt->precision = va_arg(ap, size_t);
+		opt->bits |= OPT_HAS_PRECISION;
+		return ;
+	}
 	precision = 0;
 	while (ft_isdigit(*s))
 		precision = precision * 10 + *s++ - '0';
--- src/printf/__parse_width.c
+++ src/printf/__parse_width.c
@@ -6,18 +6,25 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 08:18:49 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/17 10:15:06 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:04:42 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/options.h"
 #include "ft.h"
 
-void	__parse_width(t_opt *opt, char const **fmt)
+void	__parse_width(t_opt *opt, va_list ap, char const **fmt)
 {
 	char const	*s = *fmt;
 	size_t		width;
 
+	if (*s == '*')
+	{
+		*fmt = s + 1;
+		opt->min_width = va_arg(ap, size_t);
+		opt->bits |= OPT_HAS_WIDTH;
+		return ;
+	}
 	width = 0;
 	while (ft_isdigit(*s))
 		width = width * 10 + *s++ - '0';
