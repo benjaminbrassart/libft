11a756203435cccc0ee86fe52f6d2e1d7137fa50
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-09 19:52:10 +0200

    Add all parsing functions to `__parse_options`

--- private/options.h
+++ private/options.h
@@ -6,13 +6,14 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 07:58:02 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/05/09 16:47:13 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:40:35 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OPTIONS_H
 # define OPTIONS_H
 
+# include <stdarg.h>
 # include <stddef.h>
 
 # define OPT_NONE 0x0
@@ -48,15 +49,15 @@ void
 __parse_flags(t_opt *opt, char const **fmt);
 
 void
-__parse_width(t_opt *opt, char const **fmt);
+__parse_width(t_opt *opt, va_list ap, char const **fmt);
 
 void
-__parse_precision(t_opt *opt, char const **fmt);
+__parse_precision(t_opt *opt, va_list ap, char const **fmt);
 
 void
 __parse_length(t_opt *opt, char const **fmt);
 
 void
-__parse_options(t_opt *opt, char const **fmt);
+__parse_options(t_opt *opt, va_list ap, char const **fmt);
 
 #endif // OPTIONS_H
--- src/printf/__parse_options.c
+++ src/printf/__parse_options.c
@@ -6,14 +6,18 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 08:15:16 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/17 08:22:42 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:42:57 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "private/options.h"
 
-void	__parse_options(t_opt *opt, char const **fmt)
+void	__parse_options(t_opt *opt, va_list ap, char const **fmt)
 {
 	opt->length = LEN_NORMAL;
+	opt->bits = OPT_NONE;
+	__parse_flags(opt, fmt);
+	__parse_width(opt, ap, fmt);
+	__parse_precision(opt, ap, fmt);
 	__parse_length(opt, fmt);
 }
