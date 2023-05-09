ea7c62bd5d1337beb976ebe52681d84f3bf5142c
Benjamin Brassart <bbrassar@student.42.fr>
2023-05-09 19:52:32 +0200

    Use global decimal base for `u` conversion

--- src/printf/__print_uint.c
+++ src/printf/__print_uint.c
@@ -6,17 +6,16 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 05:52:24 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/01/17 08:27:57 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:35:04 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
 #include "private/cast.h"
+#include "private/conversions.h"
 #include "private/ft_printf.h"
 #include <unistd.h>
 #include <stdint.h>
-
-static char const	g_base[] = "0123456789";
 
 int	__print_uint(t_printerface *pi, t_opt *opt, va_list ap)
 {
@@ -25,6 +24,6 @@ int	__print_uint(t_printerface *pi, t_opt *opt, va_list ap)
 	unsigned int	n;
 
 	n = __cast_unsigned(opt->length, va_arg(ap, unsigned long long));
-	len = __uitoa_base_s(n, buffer, sizeof (buffer), g_base);
+	len = __uitoa_base_s(n, buffer, sizeof (buffer), g_base_dec);
 	return (__printerface_write(pi, buffer, len));
 }
