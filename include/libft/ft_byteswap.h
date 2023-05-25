16a8de116d62bef7fac6d0617544464ba7f4bf2a
Benjamin Brassart <benjaminbrassart@outlook.com>
2023-05-25 08:19:39 +0200

    Remove `inline` keywork for ft_bswap

--- include/libft/ft_byteswap.h
+++ include/libft/ft_byteswap.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 08:02:02 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/05/25 08:06:30 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/25 08:18:03 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 # include <stdint.h>
 
-inline uint16_t
+uint16_t
 ft_bswap_16(uint16_t bytes);
 
-inline uint32_t
+uint32_t
 ft_bswap_32(uint32_t bytes);
 
-inline uint64_t
+uint64_t
 ft_bswap_64(uint64_t bytes);
 
 #endif // FT_BYTESWAP_H
--- src/ft_bswap.c
+++ src/ft_bswap.c
@@ -6,24 +6,24 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 08:01:28 by bbrassar          #+#    #+#             */
-/*   Updated: 2023/05/25 08:14:22 by bbrassar         ###   ########.fr       */
+/*   Updated: 2023/05/25 08:18:15 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_byteswap.h"
 #include <stdint.h>
 
-inline uint16_t	ft_bswap_16(uint16_t bytes)
+uint16_t	ft_bswap_16(uint16_t bytes)
 {
 	return ((bytes >> 8) | (bytes << 8));
 }
 
-inline uint32_t	ft_bswap_32(uint32_t bytes)
+uint32_t	ft_bswap_32(uint32_t bytes)
 {
 	return (((uint32_t)ft_bswap_16(bytes)) << 16 | ft_bswap_16(bytes >> 16));
 }
 
-inline uint64_t	ft_bswap_64(uint64_t bytes)
+uint64_t	ft_bswap_64(uint64_t bytes)
 {
 	return (((uint64_t)ft_bswap_32(bytes)) << 32 | ft_bswap_32(bytes >> 32));
 }
